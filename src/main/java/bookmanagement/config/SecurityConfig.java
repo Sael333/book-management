addee622c9fe852e863a72d29682350a15f05c84
Sam <sael12@hotmail.es>
2025-08-26 15:40:31 +0200

    add allowed origin

--- src/main/java/bookmanagement/config/SecurityConfig.java
+++ src/main/java/bookmanagement/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package bookmanagement.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,60 +8,55 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @EnableWebMvc
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
-
+public class SecurityConfig implements WebMvcConfigurer{
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Value("${application.security.allowed-origins}")
-    private String allowedOrigins;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Configura las reglas de seguridad
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(new AntPathRequestMatcher("/v1/create-checkout-session")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/v1/token")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v1/token")).permitAll()// Permitir acceso sin autenticación a /v1/process-payment
                         .requestMatchers(new AntPathRequestMatcher("/v1/checkBoxOfficeAvailable")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/v1/confirm-payment")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/v1/pickupLuggage")).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated()  // Requiere autenticación para otros endpoints
                 )
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/v1/**")))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .cors(Customizer.withDefaults())  // Habilitar CORS
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/v1/**"))  // Deshabilitar CSRF solo para las rutas de /v1/**
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // Añadir filtro de autenticación JWT antes de la autenticación estándar
         return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        List<String> origins = Arrays.stream(allowedOrigins.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
-
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(origins);
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedHeaders(List.of("*"));
-        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("https://rooms-locker-dev.onrender.com"));  // Configura los orígenes permitidos
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Métodos permitidos
+        corsConfiguration.setAllowedHeaders(List.of("*"));  // Permite todas las cabeceras
+        corsConfiguration.setAllowCredentials(true);  // Permite el uso de credenciales
 
+        // Configura CORS para todas las rutas
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
+
         return source;
     }
 }
